963a3120740d7f3967a18e4da4e27dcb87b45a37
Simeon Adesanya <simeon.j.a@outlook.com>
2025-02-17 19:44:32 +0000

    modified primary to composite keys

--- database/schema.sql
+++ database/schema.sql
@@ -3,38 +3,37 @@ DROP DATABASE IF EXISTS brawl;
 CREATE DATABASE brawl;
 
 \c brawl;
-
 CREATE TABLE brawler (
-    brawler_id INT GENERATED ALWAYS AS IDENTITY,
+    brawler_id INT NOT NULL,
     brawler_version SMALLINT NOT NULL,
     brawler_name TEXT NOT NULL,
     created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
-    PRIMARY KEY (brawler_id)
+    updated_at DATETIME,
+    PRIMARY KEY (brawler_id, brawler_version)
 );
 
 CREATE TABLE starpower (
-    starpower_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+    starpower_id INT NOT NULL,
     starpower_version SMALLINT NOT NULL,
     brawler_id INT NOT NULL,
     brawler_version SMALLINT NOT NULL,
     starpower_name TEXT NOT NULL,
     created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
-    PRIMARY KEY (starpower_id),
+    updated_at DATETIME,
+    PRIMARY KEY (starpower_id, starpower_version),
     FOREIGN KEY (brawler_id) REFERENCES brawler (brawler_id),
     FOREIGN KEY (brawler_version) REFERENCES brawler (brawler_version)
 );
 
 CREATE TABLE gear (
-    gear_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+    gear_id INT NOT NULL,
     gear_version SMALLINT NOT NULL,
     brawler_id INT NOT NULL,
     brawler_version SMALLINT NOT NULL,
     gear_name TEXT NOT NULL,
     created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
-    PRIMARY KEY (gear_id),
+    updated_at DATETIME,
+    PRIMARY KEY (gear_id, gear_version),
     FOREIGN KEY (brawler_id) REFERENCES brawler (brawler_id),
     FOREIGN KEY (brawler_version) REFERENCES brawler (brawler_version)
 );
