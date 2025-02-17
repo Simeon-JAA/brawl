1d9a97a5cd12a30e389f0b27d93e03d40e7f28e9
Simeon Adesanya <simeon.j.a@outlook.com>
2025-02-17 19:07:28 +0000

    create tables for star powers and gears

--- database/schema.sql
+++ database/schema.sql
@@ -5,10 +5,36 @@ CREATE DATABASE brawl;
 \c brawl;
 
 CREATE TABLE brawler (
-    brawler_id SMALLINT GENERATED ALWAYS AS IDENTITY,
-    brawler_name TEXT NOT NULL,
+    brawler_id INT GENERATED ALWAYS AS IDENTITY,
     brawler_version SMALLINT NOT NULL,
+    brawler_name TEXT NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
     PRIMARY KEY (brawler_id)
+);
+
+CREATE TABLE starpower (
+    starpower_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+    starpower_version SMALLINT NOT NULL,
+    brawler_id INT NOT NULL,
+    brawler_version SMALLINT NOT NULL,
+    starpower_name TEXT NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY (starpower_id),
+    FOREIGN KEY (brawler_id) REFERENCES brawler (brawler_id),
+    FOREIGN KEY (brawler_version) REFERENCES brawler (brawler_version)
+);
+
+CREATE TABLE gear (
+    gear_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+    gear_version SMALLINT NOT NULL,
+    brawler_id INT NOT NULL,
+    brawler_version SMALLINT NOT NULL,
+    gear_name TEXT NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY (gear_id),
+    FOREIGN KEY (brawler_id) REFERENCES brawler (brawler_id),
+    FOREIGN KEY (brawler_version) REFERENCES brawler (brawler_version)
 );
