42f97d8676d3ff3b5ad0b8ff934b124be411707d
Simeon Adesanya <simeon.j.a@outlook.com>
2025-02-24 11:49:32 +0000

    create load.py sleleton

--- etl/load.py
+++ etl/load.py
@@ -1,13 +1,51 @@
 """Load file for loading chages into database"""
 
+from os import environ
+
 import psycopg2
 from psycopg2.extensions import connection
+from dotenv import load_dotenv
 
 def get_db_connection(config) -> connection:
     """Establishes connection with the database"""
+
+    try:
+        conn = psycopg2.connect(dbname = config["dbname"],
+                         user = config["user"],
+                         password = config["password"],
+                         host = config["host"],
+                         port = config["port"]
+        )
+
+    except:
+        raise ConnectionError("Error: Cannot establish connection to database")
+    
+    return conn
+
+
+def insert_new_brarler_data(conn: connection, brawler_data: dict):
+    """Insterts new brawler data into the database"""
+
+    with conn.cursor() as cur:
+        try:
+            
+            cur.execute("""""")
+
+        except:
+            raise ConnectionError("Error: Unable to insert data")
+
     return
 
 
 if __name__ =="__main__":
+   
+    load_dotenv()
 
-   get_db_connection("Input")
+    config = environ
+
+    conn = get_db_connection(config)
+
+    insert_new_brarler_data(conn, "temp")
+
+    conn.close()
+        
