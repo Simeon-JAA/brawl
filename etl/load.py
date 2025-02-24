f9dfe9013e206753ae8ae59267ad6ea9b566ee8e
Simeon Adesanya <simeon.j.a@outlook.com>
2025-02-24 11:49:13 +0000

    add pyscopg2 to requirements

---
+++ etl/load.py
@@ -0,0 +1,13 @@
+"""Load file for loading chages into database"""
+
+import psycopg2
+from psycopg2.extensions import connection
+
+def get_db_connection(config) -> connection:
+    """Establishes connection with the database"""
+    return
+
+
+if __name__ =="__main__":
+
+   get_db_connection("Input")
