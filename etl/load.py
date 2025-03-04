96921021cdf368b398fb92860e18bea193d38647
Simeon Adesanya <simeon.j.a@outlook.com>
2025-03-04 16:49:44 +0000

    etl/load.py - pylint improvements

--- etl/load.py
+++ etl/load.py
@@ -6,39 +6,44 @@ import psycopg2
 from psycopg2.extensions import connection
 from dotenv import load_dotenv
 
-def get_db_connection(config) -> connection:
+def get_db_connection(config_env) -> connection:
     """Establishes connection with the database"""
 
     try:
-        conn = psycopg2.connect(dbname = config["dbname"],
-                         user = config["user"],
-                         password = config["password"],
-                         host = config["host"],
-                         port = config["port"]
+        db_conn = psycopg2.connect(dbname = config_env["dbname"],
+                         user = config_env["user"],
+                         password = config_env["password"],
+                         host = config_env["host"],
+                         port = config_env["port"]
         )
 
-    except:
-        raise ConnectionError("Error: Cannot establish connection to database")
-    
-    return conn
+    except Exception as exc:
+        raise ConnectionError("Error: Cannot establish connection to database!") from exc
+
+    return db_conn
 
 
-def insert_new_brarler_data(conn: connection, brawler_data: dict):
+def insert_new_brarler_data(db_conn: connection, brawler_data: dict):
     """Inserts new brawler data into the database"""
 
-    with conn.cursor() as cur:
+    if not isinstance(brawler_data, dict):
+        raise TypeError("Error: Brawler data is not a dictionary!")
+
+    if not brawler_data:
+        raise ValueError("Error: Brawler data cannot be empty!")
+
+    with db_conn.cursor() as cur:
         try:
-            
+
             cur.execute("""""")
 
-        except:
-            raise ConnectionError("Error: Unable to insert data")
+        except Exception as exc:
+            raise ConnectionError("Error: Unable to insert data!") from exc
 
-    return
 
 
 if __name__ =="__main__":
-   
+
     load_dotenv()
 
     config = environ
@@ -48,4 +53,3 @@ if __name__ =="__main__":
     insert_new_brarler_data(conn, "temp")
 
     conn.close()
-        
